8e2053f5eefe4a82951c5a5d51acf0ab100e4596
Usman <Usman@USMAN>
2017-03-31 17:19:28 +0100

    create test and make it red.

---
+++ CheckoutKata.Tests/CheckoutTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CheckoutKata.Tests
+{
+    [TestClass]
+    public class CheckoutTests
+    {
+        private List<Product> _products;
+
+        [TestMethod]
+        public void ScanOneItem()
+        {
+            string sku = "a";
+            var checkout = new Checkout();
+
+            checkout.Scan(product);
+
+            var product = _products.FirstOrDefault(x => x.SKU == sku);
+            Assert.IsNotNull(product);
+            Assert.AreEqual(1, product.TimesScanned);
+        }
+    }
+}
